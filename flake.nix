a304b134780fced8bec6a896d81e2099745838e5
Ryan Walker <ryanjwalker2001@gmail.com>
2025-05-09 12:56:56 +0100

    git and flake init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: (forSystem system f));
+
+    forSystem = system: f:
+      f rec {
+        inherit system;
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
+  in {
+    devShells = forAllSystems ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        buildInputs = builtins.attrValues {
+          inherit
+            (pkgs)
+            cargo
+            rustc
+            rustfmt
+            cargo-watch
+            spacetimedb
+            ;
+        };
+      };
+    });
+  };
+}
